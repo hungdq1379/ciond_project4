b55c860bbd9a8a2dd249f0d205d8536baf18d9f1
hungdq13 <hungdq13@gmail.com>
2022-11-07 11:50:38 -0500

    modify app.py,make_predition, docker_out.txt

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
+logging.basicConfig(filename='output_txt_files/docker_out.txt',level=logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+#dockerpath=<your docker ID/path>
+dockerpath=hungdq1379/docker1379/latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u hungdq1379 -p dckr_pat_KezEZsXMgT-S3z4czOKQJk9dOlA
+docker tag ciond_project4:v1.0 hungdq1379/docker1379:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push hungdq1379/docker1379:latest
