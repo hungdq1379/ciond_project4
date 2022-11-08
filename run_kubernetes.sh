51dffdca54b4739b25f0ac1bfbd3456df11ef06a
hung.duong <hung.duong@commbank.local>
2022-11-08 22:02:35 +0700

    Submit files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=hungdq1379/docker1379/latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl apply -f deployment.yaml
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl apply -f service.yaml
