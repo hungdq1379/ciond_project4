ad2640c4e611bc3948030b5cf1f63beab10fe6c5
hungdq1379 <113217676+hungdq1379@users.noreply.github.com>
2022-11-11 23:14:51 +0700

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,12 +8,16 @@ dockerpath=hungdq1379/docker1379/latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl apply -f deployment.yaml
+#kubectl apply -f deployment.yaml
+kubectl run webForProject4\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=webForProject4
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward flaskskearlndemo 8000:80
-kubectl apply -f service.yaml
+kubectl port-forward webForProject4 8080:80
+#kubectl apply -f service.yaml
